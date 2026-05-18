7e3fe99a471dca542c18442deb671729ba458c1a
Navva Srihitha <srihitha.navva@gmail.com>
2026-05-18 14:20:54 +0530

    Update allowed origins for CORS configuration

--- Blog-Backend/server.js
+++ Blog-Backend/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
-  'https://capstone-project-srihithanavva-6061s-projects.vercel.app',
+  'https://capstone-project-taupe-alpha.vercel.app',
 ]
 
 app.use(cors({

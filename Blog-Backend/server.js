925604068b988de611dc4be3a8a851f43255cd01
Navva Srihitha <srihitha.navva@gmail.com>
2026-05-18 13:51:31 +0530

    Fix CORS origin URL for production

--- Blog-Backend/server.js
+++ Blog-Backend/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
-  'capstone-project-srihithanavva-6061s-projects.vercel.app',
+  'http://capstone-project-srihithanavva-6061s-projects.vercel.app',
 ]
 
 app.use(cors({
